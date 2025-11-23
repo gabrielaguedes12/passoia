847189a7a37df5547e36495f078760bd2c61a045
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-11-23 11:46:01 -0300

    icones

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import "../Header/header.scss";
 import logo from "../../assets/header/logo.png";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { byPrefixAndName } from "@fortawesome/fontawesome-svg-core/import.macro";
+
 export default function Header() {
 
   return (
@@ -11,6 +14,12 @@ export default function Header() {
         <li>LOOKS</li>
         <li>LANÇAMENTOS</li>
         <li>NOVIDADES</li>
+        <li className="icon-cart">
+          <FontAwesomeIcon 
+            icon={byPrefixAndName.fas["cart-shopping"]}
+            className="cartIcon"
+          />
+        </li>
       </ul>
     </header>
   );
