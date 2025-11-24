d0284fce607826ffc68bdcc09dbda06608f4fd95
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-11-24 12:36:17 -0300

    melhorias no header e footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,6 +14,7 @@ export default function Footer(){
     return(
         <>
         <section className="footer-container">
+
         <div className="cardFooter">
         <h2 className="tituloFooter">ATENDIMENTO</h2>
         <p>Fale Conosco</p>
@@ -22,21 +23,27 @@ export default function Footer(){
         <p>Nossas Lojas</p>
         </div>
 
-        <div className="cardFooter">
+      <div className="cardFooter">
         <h2 className="tituloFooter">FORMAS DE PAGAMENTO</h2>
-        <img className="boleto" src={boleto} alt="boleto" />      
-      <img className="mastercard" src={mastercard} alt="mastercard" />      
-      <img className="pix" src={pix} alt="pix" />      
-      <img className="visa" src={visa} alt="visa" />  
+
+        <div className="pagamentos-container">
+           <img  src={boleto} alt="boleto" />      
+           <img  src={mastercard} alt="mastercard" />      
+           <img src={pix} alt="pix" />      
+           <img  src={visa} alt="visa" />  
         </div>
+      </div>
         
-        <div className="cardFooter">
+      <div className="cardFooter">
         <h2 className="tituloFooter">SIGA-NOS NAS REDES SOCIAIS</h2>
-        <img className="instagram" src={instagram} alt="instagram" />      
-        <img className="facebook" src={facebook} alt="facebook" />      
-        <img className="youtube" src={youtube} alt="youtube" />      
-        <img className="twitter" src={twitter} alt="twitter" />  
-             </div>
+
+        <div className="redes-container">
+          <img  src={instagram} alt="instagram" />      
+          <img  src={facebook} alt="facebook" />      
+           <img  src={youtube} alt="youtube" />      
+          <img  src={twitter} alt="twitter" />  
+        </div>
+      </div>
      </section>
         </>
     )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,56 @@
 import "../Header/header.scss";
 import logo from "../../assets/header/logo.png";
+import { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faCartShopping,faBars, faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Header() {
+  const [darkMode, setDarkMode] = useState(false);
+  const[menuOpen, setMenuOpen] = useState(false);
+
+  function toggleTheme() {
+    setDarkMode(!darkMode);
+  }
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
 
   return (
     <header>
       <img className="logoImg" src={logo} alt="logo" />
 
-      <ul>
+      <ul className={menuOpen? "menu open" : "menu"}>
         <li>LOOKS</li>
         <li>LANÇAMENTOS</li>
         <li>NOVIDADES</li>
-        <li className="icon-cart">
-            <FontAwesomeIcon icon={faCartShopping} className="cartIcon" />
-        </li>
-      </ul>
+        <li>NOVIDADES</li>
+        <li>NOVIDADES</li>
+        <li>NOVIDADES</li>
+        <li>NOVIDADES</li>
+        <li>NOVIDADES</li>
+        </ul>
+
+        <div className="icons">       
+          <FontAwesomeIcon 
+          icon={darkMode ? faMoon : faSun} 
+          className="icon" 
+          onClick={toggleTheme}
+          />
+
+
+          <FontAwesomeIcon icon={faCartShopping} className="icon" />
+          <FontAwesomeIcon icon={faUser} className="icon" />
+          
+          <FontAwesomeIcon 
+          icon={faBars} 
+          className="icon menu-icon"
+          onClick={toggleMenu} 
+          />        
+      </div>
     </header>
   );
 }
